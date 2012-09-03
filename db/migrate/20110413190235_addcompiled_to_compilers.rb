da7184aa94e065bd5e9df5d718e597a7573cfc4f
Artem Shoobovych <shybovycha@gmail.com>
2012-09-03 14:38:06 +0300

    Updated README. Added migration to populate the database with at least one sample task.

--- db/migrate/20110413190235_addcompiled_to_compilers.rb
+++
@@ -1,7 +0,0 @@
-class AddcompiledToCompilers < ActiveRecord::Migration
-  def self.up
-  end
-
-  def self.down
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110413190600) do
+ActiveRecord::Schema.define(:version => 20120903111846) do
 
   create_table "compilers", :force => true do |t|
     t.string   "title"
